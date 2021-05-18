ddb8c07d324370b7a1cb8805d06be6d20e04bb3d
Jongchan Lee <jc@JongchannoMac-mini.local>
2021-05-19 00:25:56 +0900

    regression variable selection start

--- R/R3ProjRcode.R
+++ R/R3ProjRcode.R
@@ -59,243 +59,85 @@ name <- function(x) {
   !is.na(x)
 }
 d_mf<-d[, sapply(d[48,], name)]
-d_mf %>% DT::datatable()
 d_mf %>% dim()
 d_m<-d_mf %>% filter(sex=="M")
 d_f<-d_mf %>% filter(sex=="F")
 
 
-# summary(d_mf)
-
-
+## --------------------------------------------------
+d_m
 
 ## --------------------------------------------------
-d_m %>% DT::datatable()
-
-
-## --------------------------------------------------
-d_f %>% DT::datatable()
-
+d_f
 
 ## --------------------------------------------------
-d_common %>% colnames() %>% enframe() %>% DT::datatable()
+d_common %>% colnames() %>% enframe()
+
+# ----------------- ----------------- ----------------- -----------------
+#      _       _          _                            _                    _
+#    _| | __ _| |_ __ _  (_)_ __ ___  _ __   ___  _ __| |_    ___ _ __   __| |
+#  / _` |/ _` | __/ _` | | | '_ ` _ \| '_ \ / _ \| '__| __|  / _ \ '_ \ / _` |
+# | (_| | (_| | || (_| | | | | | | | | |_) | (_) | |  | |_  |  __/ | | | (_| |
+#  \__,_|\__,_|\__\__,_| |_|_| |_| |_| .__/ \___/|_|   \__|  \___|_| |_|\__,_|
+#                                    |_|
+# ----------------- ----------------- ----------------- -----------------
 
 
-## --------------------------------------------------
-d_m %>% colnames() %>% enframe() %>% DT::datatable()
-
-
-
-## --------------------------------------------------
-d_f %>% colnames() %>% enframe() %>% DT::datatable()
-
-
-## ---- warning=FALSE--------------------------------
-suppressMessages(library(plotly))
-
-t<-d_m %>% select(pref.J,HLE_2016,LE_2015) %>%
-  mutate(pref.J=forcats::fct_reorder(pref.J, LE_2015))
-t$pref.J
-
-t %>%
-  plot_ly() %>%
-  add_segments(
-    x=~HLE_2016,y=~pref.J ,
-    xend=~LE_2015,yend=~pref.J ,
-    # x = ~c, y = ~model,
-    # xend = ~h, yend = ~model,
-    color = I("gray"), showlegend = FALSE
-  ) %>%
-  add_markers(
-    # x = ~c, y = ~model,
-    x=~HLE_2016,y=~pref.J ,
-    color = I("blue"),
-    name = "健康寿命"
-  ) %>%
-  add_markers(
-    # x = ~h, y = ~model,
-    x=~LE_2015,y=~pref.J ,
-    color = I("red"),
-    name  = "平均寿命"
-  ) %>%
-  layout(
-    xaxis = list(
-      range=c(60,83),
-      title="男性の平均寿命と健康寿命の差"
-    )
-  )
+# ----------------- ----------------- ----------------- -----------------
+# ____             _
+# |  _ \ __ _ _ __ | | __
+# | |_) / _` | '_ \| |/ /
+# |  _ < (_| | | | |   <
+# |_| \_\__,_|_| |_|_|\_\
+# ----------------- ----------------- ----------------- -----------------
 
 
 
-## ---- warning=FALSE--------------------------------
-
-suppressMessages(library(plotly))
-
-t<-d_f %>% select(pref.J,HLE_2016,LE_2015) %>%
-  mutate(pref.J=forcats::fct_reorder(pref.J, LE_2015))
-t$pref.J
-
-t %>%
-  # plot_ly(width = 600, height = 1000) %>%
-  plot_ly() %>%
-  add_segments(
-    x=~HLE_2016,y=~pref.J ,
-    xend=~LE_2015,yend=~pref.J ,
-    # x = ~c, y = ~model,
-    # xend = ~h, yend = ~model,
-    color = I("gray"), showlegend = FALSE
-  ) %>%
-  add_markers(
-    # x = ~c, y = ~model,
-    x=~HLE_2016,y=~pref.J ,
-    color = I("blue"),
-    name = "健康寿命"
-  ) %>%
-  add_markers(
-    # x = ~h, y = ~model,
-    x=~LE_2015,y=~pref.J ,
-    color = I("red"),
-    name  = "平均寿命"
-  ) %>%
-  layout(
-    xaxis = list(
-      range=c(60,88),
-      title="女性の平均寿命と健康寿命の差"
-    )
-  )
-
-
-
-## --------------------------------------------------
-d_m %>% select(LE_2015) %>% bind_cols(Wakayama::pref["pref.J"]) %>% dplyr::arrange(LE_2015) %>% mutate(rank=row_number()) %>%DT::datatable()
-
-
-## --------------------------------------------------
-jc.dotplot <- function(x) {
-  # x is HLE or LE in c()　vector
-  names(x)<-Wakayama::pref$pref.J
-  x<-x[order(x)]
-
-}
-
-colfunc <- colorRampPalette(c("gray90","black"))
-
-par(family= "HiraKakuProN-W3")
-
-dotchart(
-  main = "平均寿命(2015年, 男性)",
-  jc.dotplot(d_m$LE_2015),
-  cex = 0.7,
-  lcolor = "gray90",
-  pch = 19,
-  col = colfunc(47),
-  pt.cex = 1.5
-)
-
-abline(v = 79, lty = 2)
-
-
-
-## --------------------------------------------------
-d_m %>% select(HLE_2016) %>% bind_cols(Wakayama::pref["pref.J"]) %>% dplyr::arrange(HLE_2016) %>% mutate(rank=row_number()) %>%DT::datatable()
-
-
-## --------------------------------------------------
-jc.dotplot <- function(x) {
-  # x is HLE or LE in c()　vector
-  names(x)<-Wakayama::pref$pref.J
-  x<-x[order(x)]
-
-}
-
-colfunc <- colorRampPalette(c("gray90","black"))
-
-par(family= "HiraKakuProN-W3")
-
-dotchart(
-  main = "健康寿命(2016年, 男性)",
-  jc.dotplot(d_m$HLE_2016),
-  cex = 0.7,
-  lcolor = "gray90",
-  pch = 19,
-  col = colfunc(47),
-  pt.cex = 1.5
-)
-
-abline(v = 79, lty = 2)
-
-
-
-## --------------------------------------------------
-d_f %>% select(LE_2015) %>% bind_cols(Wakayama::pref["pref.J"]) %>% dplyr::arrange(LE_2015) %>% mutate(rank=row_number()) %>%DT::datatable()
-
-
-## --------------------------------------------------
-jc.dotplot <- function(x) {
-  # x is HLE or LE in c()　vector
-  names(x)<-Wakayama::pref$pref.J
-  x<-x[order(x)]
-
-}
-
-colfunc <- colorRampPalette(c("gray90","black"))
-
-par(family= "HiraKakuProN-W3")
-
-dotchart(
-  main = "平均寿命(2015年, 女性)",
-  jc.dotplot(d_f$LE_2015),
-  cex = 0.7,
-  lcolor = "gray90",
-  pch = 19,
-  col = colfunc(47),
-  pt.cex = 1.5
-)
-
-abline(v = 79, lty = 2)
-
-
-
-## --------------------------------------------------
-d_f %>% select(HLE_2016) %>% bind_cols(Wakayama::pref["pref.J"]) %>% dplyr::arrange(HLE_2016) %>% mutate(rank=row_number()) %>%DT::datatable()
-
-
-## --------------------------------------------------
-jc.dotplot <- function(x) {
-  # x is HLE or LE in c()　vector
-  names(x)<-Wakayama::pref$pref.J
-  x<-x[order(x)]
-
-}
-
-colfunc <- colorRampPalette(c("gray90","black"))
-
-par(family= "HiraKakuProN-W3")
-
-dotchart(
-  main = "平均寿命(2015年, 女性)",
-  jc.dotplot(d_f$HLE_2016),
-  cex = 0.7,
-  lcolor = "gray90",
-  pch = 19,
-  col = colfunc(47),
-  pt.cex = 1.5
-)
-
-abline(v = 79, lty = 2)
-
-
-
-## --------------------------------------------------
+## 和歌山県順位 with common data--------------------------------------------------
 d_common %>% dplyr::select_if(is.numeric) %>% sapply(rank) %>%
-  tbl_df() %>%dplyr::filter(key==30) %>% t() %>% as.data.frame() %>% DT::datatable()
+  tbl_df() %>%dplyr::filter(key==30) %>% t() %>% as.data.frame() %>%
+  rownames_to_column() %>% tbl_df() %>%
+  left_join(var, by=c("rowname"="var_name_Eng")) %>%
+  # select(var_name_Jpn, V1, everything())
+  select(var_name_Jpn, V1) %>%
+  mutate(rank=V1) %>%select(-V1) %>% filter(rank<5 | rank>42)
 
 
-## ---- warning=FALSE--------------------------------
 
-# purrr::map(iris[,-5], ~hist(.x))
+## 和歌山県順位 with d_f data--------------------------------------------------
+d_f %>% dplyr::select_if(is.numeric) %>% sapply(rank) %>%
+  tbl_df() %>%dplyr::filter(key==30) %>% t() %>% as.data.frame() %>%
+  rownames_to_column() %>% tbl_df() %>%
+  left_join(var, by=c("rowname"="var_name_Eng")) %>%
+  # select(var_name_Jpn, V1, everything())
+  select(var_name_Jpn, V1) %>%
+  mutate(rank=V1) %>%select(-V1) %>% filter(rank<5 | rank>42)
 
-suppressMessages(library(plotly))
+
+## 和歌山県順位 with d_m data--------------------------------------------------
+d_m %>% dplyr::select_if(is.numeric) %>% sapply(rank) %>%
+  tbl_df() %>%dplyr::filter(key==30) %>% t() %>% as.data.frame() %>%
+  rownames_to_column() %>% tbl_df() %>%
+  left_join(var, by=c("rowname"="var_name_Eng")) %>%
+  # select(var_name_Jpn, V1, everything())
+  select(var_name_Jpn, V1) %>%
+  mutate(rank=V1) %>%select(-V1) %>% filter(rank<5 | rank>42)
+
+
+# ----------------- ----------------- ----------------- -----------------
+# ____  _                  _               _ _          _   _
+# / ___|| |_ __ _ _ __   __| | __ _ _ __ __| (_)______ _| |_(_) ___  _ __
+# \___ \| __/ _` | '_ \ / _` |/ _` | '__/ _` | |_  / _` | __| |/ _ \| '_ \
+#  ___) | || (_| | | | | (_| | (_| | | | (_| | |/ / (_| | |_| | (_) | | | |
+# |____/ \__\__,_|_| |_|\__,_|\__,_|_|  \__,_|_/___\__,_|\__|_|\___/|_| |_|
+#
+# ----------------- ----------------- ----------------- -----------------
+
+## d_common 標準化--------------------------------------------------
+# d_common_standarize
+d_common %>% map_df(typeof) %>% t()
+d_m %>% map_df(typeof) %>% t()
+d_f %>% map_df(typeof) %>% t()
 
 
 d_common_standarize<-
@@ -303,6 +145,83 @@ d_common_standarize<-
   select(-key, -pref.id) %>% scale() %>% tbl_df()
 
 d_common_standarize<-bind_cols(d_common["pref.id"], d_common_standarize)
+
+
+
+# ----------------- ----------------- ----------------- -----------------
+# ____                              _
+# |  _ \ ___  __ _ _ __ ___  ___ ___(_) ___  _ __
+# | |_) / _ \/ _` | '__/ _ \/ __/ __| |/ _ \| '_ \
+# |  _ <  __/ (_| | | |  __/\__ \__ \ | (_) | | | |
+# |_| \_\___|\__, |_|  \___||___/___/_|\___/|_| |_|
+#            |___/
+# ----------------- ----------------- ----------------- -----------------
+#
+# d_f data 変数選択　and regression
+
+
+aa=d_f %>% select(-key, -pref.id,  -pref.E, -pref.A, -pref.J, -sex, -pop) %>%
+  select(LE_2015,everything()) %>% select(-HLE_2016)
+
+
+bbb=glm(LE_2015~., data=aa[,1:30])
+
+cc=step(bbb, direction = "backward")
+
+cc %>% broom::tidy()
+
+#
+#  _____           _
+# |_   _|__     __| | ___
+#   | |/ _ \   / _` |/ _ \
+#   | | (_) | | (_| | (_) |  _ _
+#   |_|\___/   \__,_|\___/  (_|_)
+#
+
+
+## ---- warning=FALSE--------------------------------
+jc_shapiro <- function(x) {
+  shapiro.test(x) %>% broom::tidy() %>% select(p.value) %>% round(3)
+}
+
+
+d_common_normality_test<-d_common %>% dplyr::select_if(is.numeric) %>%
+  select(-key, -pref.id) %>%
+  # scale() %>%
+
+
+
+
+
+
+
+## ---- warning=FALSE--------------------------------
+jc_shapiro <- function(x) {
+  shapiro.test(x) %>% broom::tidy() %>% select(p.value) %>% round(3)
+}
+
+
+d_common_normality_test<-d_common %>% dplyr::select_if(is.numeric) %>%
+  select(-key, -pref.id) %>%
+  # scale() %>%
+  tbl_df() %>% purrr::map_df(jc_shapiro)
+
+rownames(d_common_normality_test)<-d_common %>% dplyr::select_if(is.numeric) %>%
+  select(-key, -pref.id) %>% colnames()
+
+DT::datatable(d_common_normality_test)
+
+
+## --------------------------------------------------
+d_common_normality_test %>% filter(p.value<0.05) %>% DT::datatable()
+
+
+
+
+
+
+
+
 
 
 d_common_standarize_long<-
@@ -351,28 +270,6 @@ d_common_standarize_long_density %>%
 
 
 
-
-## ---- warning=FALSE--------------------------------
-jc_shapiro <- function(x) {
-  shapiro.test(x) %>% broom::tidy() %>% select(p.value) %>% round(3)
-}
-
-
-d_common_normality_test<-d_common %>% dplyr::select_if(is.numeric) %>%
-  select(-key, -pref.id) %>%
-  # scale() %>%
-  tbl_df() %>% purrr::map_df(jc_shapiro)
-
-rownames(d_common_normality_test)<-d_common %>% dplyr::select_if(is.numeric) %>%
-  select(-key, -pref.id) %>% colnames()
-
-DT::datatable(d_common_normality_test)
-
-
-## --------------------------------------------------
-d_common_normality_test %>% filter(p.value<0.05) %>% DT::datatable()
-
-
 ## --------------------------------------------------
 library(plotly)
 temp<-left_join(d_common_standarize_long, d_common,
@@ -392,5 +289,4 @@ temp %>% filter(pref.J=="和歌山"|
 
 
 
-
-
+ls()
